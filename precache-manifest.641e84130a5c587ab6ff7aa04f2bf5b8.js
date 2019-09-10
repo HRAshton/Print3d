52ffe3b42f6f2d39c72ec5fcb0df852169b180d3
Kirill Suntsov <HRAshton@yandex.com>
2019-09-11 03:38:43 +0700

    v

---
+++ precache-manifest.641e84130a5c587ab6ff7aa04f2bf5b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ae83c8d7ddfed6e010eb66dac00445c",
+    "url": "/static/media/Tetrahedron.2ae83c8d.svg"
+  },
+  {
+    "revision": "5eee3027f9028d0d1e6dff873d57799a",
+    "url": "/static/media/Parallelepiped.5eee3027.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "1cae933f299e6d95984f",
+    "url": "/static/js/main.1cae933f.chunk.js"
+  },
+  {
+    "revision": "31d5087e8c772fba2341",
+    "url": "/static/js/2.31d5087e.chunk.js"
+  },
+  {
+    "revision": "1cae933f299e6d95984f",
+    "url": "/static/css/main.4826f635.chunk.css"
+  },
+  {
+    "revision": "31d5087e8c772fba2341",
+    "url": "/static/css/2.0e57896e.chunk.css"
+  },
+  {
+    "revision": "b057f84b1a00229f773349ba490645c9",
+    "url": "/index.html"
+  }
+];
